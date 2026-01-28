ce87f973650d9081d961620b7e89ef3e4c582da4
Gregory Fernandez <gregory.fernandez@kosmos.fr>
2026-01-28 18:05:24 +0100

    Modification du Jenkinsfile pour publier le package dans npm et docker

--- scripts/docker-package.sh
+++ scripts/docker-package.sh
@@ -19,3 +19,10 @@ fi
 DIST_VERSION=$("$DIR"/normalize-version.sh "$DIST_VERSION")
 
 VERSION=$DIST_VERSION pnpm --dir apps/web build
+
+pushd /src/apps/web/webapp
+tar --exclude='..' --exclude='.' -czf ../element-web-${DIST_VERSION}.tgz * .*
+echo $NEXUS_USER
+echo $NEXUS_PASS
+curl -k -u $NEXUS_USER:$NEXUS_PASS --upload-file ../element-web-${DIST_VERSION}.tgz https://nexus.nantes.kosmos.fr/nexus/repository/kdecole.internal.raw/element-web-${DIST_VERSION}.tgz
+popd
