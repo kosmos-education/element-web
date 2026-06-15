1520524477fec08d299a8c8f4a267267f89f65af
Matthieu Bouvais <matthieu.bouvais@kosmos.fr>
2026-06-15 11:36:12 +0200

    Revue du mode de packaging pour se réadapter à la nouvelle version

--- scripts/docker-package.sh
+++ scripts/docker-package.sh
@@ -2,21 +2,14 @@
 
 set -ex
 
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
-
 DIR=$(dirname "$0")
 
-# If the branch comes out as HEAD then we're probably checked out to a tag, so if the thing is *not*
-# coming out as HEAD then we're on a branch. When we're on a branch, we want to resolve ourselves to
-# a few SHAs rather than a version.
-if [[ $BRANCH != HEAD && ! $BRANCH =~ heads/v.+ ]]
-then
-    DIST_VERSION=$("$DIR"/get-version-from-git.sh)
-else
-    DIST_VERSION=$(git describe --abbrev=0 --tags)
-fi
-
+# Version dérivée du dernier tag git (ex: v1.12.21 -> 1.12.21), suffixée -kosmos
+DIST_VERSION=$(git describe --abbrev=0 --tags)
 DIST_VERSION=$("$DIR"/normalize-version.sh "$DIST_VERSION")
+DIST_VERSION="${DIST_VERSION}-kosmos"
+
+echo $DIST_VERSION
 
 VERSION=$DIST_VERSION pnpm --dir apps/web build
 
